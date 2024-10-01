29607d733fdf5d7693a8087e276b2ef49590ad1f
Jeffrey Mathew <jeoffrey.mathews@jmgcapital.net>
2024-10-01 19:13:58 +0530

    update
    update

--- packages/ui/src/views/chatflows/EmbedChat.jsx
+++ packages/ui/src/views/chatflows/EmbedChat.jsx
@@ -180,13 +180,13 @@ const chatwindowConfig = (isReact = false) => {
                     footer: {
                         textColor: '#303235',
                         text: 'Powered by',
-                        company: 'Flowise',
-                        companyLink: 'https://flowiseai.com',
+                        company: 'Teckgeekz',
+                        companyLink: 'https://teckgeekz.com',
                     }
                 }`
         : `chatWindow: {
                 showTitle: true,
-                title: 'Flowise Bot',
+                title: 'Teckgeekz Bot',
                 titleAvatarSrc: 'https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg',
                 showAgentMessages: true,
                 welcomeMessage: 'Hello! This is custom welcome message',
